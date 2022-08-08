ccfd6c7c997d5ac67d6a3a7caae9b856c4c95846
Dangyk <18639403972@163.com>
2022-08-08 10:24:04 +0800

    third commit

--- src/atguigu/com/GitTest.java
+++ src/atguigu/com/GitTest.java
@@ -7,7 +7,8 @@ package atguigu.com;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("aaa");
-        System.out.println("aaa");
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
     }
 }
