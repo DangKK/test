08ec890cfd9799f8a07b7801ebd03964fbba8992
Dangyk <18639403972@163.com>
2022-08-08 11:53:06 +0800

    master-commit-2

--- src/atguigu/com/GitTest.java
+++ src/atguigu/com/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("hot-fix1");
         System.out.println("hot-fix2");
         System.out.println("=============================");
+        System.out.println("master123");
+
     }
 }
