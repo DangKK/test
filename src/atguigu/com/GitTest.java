1d95084f7dc70a29e3091f467117c3d018c4a895
Dangyk <18639403972@163.com>
2022-08-08 10:20:50 +0800

    first commit

---
+++ src/atguigu/com/GitTest.java
@@ -0,0 +1,12 @@
+package atguigu.com;
+
+/*
+*
+*
+* */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
---
+++ src/atguigu/com/GitTest1.java
@@ -0,0 +1,7 @@
+package atguigu.com;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
